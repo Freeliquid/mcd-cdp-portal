bc361925705396814672fbfcfad6cff988cfb596
antonpatrinika <32767432+antonpatrinika@users.noreply.github.com>
2020-09-27 01:34:29 +0800

    new css style in Layouts

--- src/layouts/SidebarActionLayout.js
+++ src/layouts/SidebarActionLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Flex, Card } from '@makerdao/ui-components-core';
 import { ReactComponent as CloseIcon } from 'images/close-simple.svg';
-
+import { getColor } from 'styles/theme';
 const fullscreenCardProps = {
   border: '0',
   borderRadius: '0'
@@ -9,7 +9,7 @@ const fullscreenCardProps = {
 
 const SidebarActionLayout = ({ onClose, children, fullscreen }) => {
   return (
-    <Card p="m" {...(fullscreen ? fullscreenCardProps : {})}>
+    <Card p="m" {...(fullscreen ? fullscreenCardProps : {})} style={{color: getColor('greyText'), backgroundColor: getColor('cardBg'), borderColor: getColor('border')}}>
       <Flex justifyContent="flex-end" pb="xs">
         <Box
           onClick={onClose}
