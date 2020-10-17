dc4574986a93981dede97ceef705a2f2ee50acc5
antonpatrinika <32767432+antonpatrinika@users.noreply.github.com>
2020-10-17 17:49:36 +0800

    update option animation

--- src/pages/AnimateConvas.js
+++ src/pages/AnimateConvas.js
@@ -5,8 +5,15 @@ import animationData from 'images/animation/PC_version.json';
 class AnimationLottie extends Component {
   render() {
     const defaultOptions = {
+      renderer: 'svg',
+      loop: true,
       autoplay: true,
-      animationData: animationData
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice',
+        progressiveLoad: false,
+        clearCanvas: false,
+      }
     };
 
     return <Lottie options={defaultOptions} height={400} width={400} />;
