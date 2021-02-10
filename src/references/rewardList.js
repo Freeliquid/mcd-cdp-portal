0bdf33fcb27ad414dbd327a56ab8fa8c23d5a968
antonpatrinika <32767432+antonpatrinika@users.noreply.github.com>
2021-02-10 22:59:51 +0800

    move to bottom in list pool USDFL_FL

--- src/references/rewardList.js
+++ src/references/rewardList.js
@@ -1,2 +1,12 @@
 
-export default [ "USDFL_FL", "USDFL_USDC", "USDFL_DAI", "USDFL_USDT", "USDFL_USDN", "FL_USDC","FL_USDT","FL_DAI","FL_USDN" ];
+export default [
+    "USDFL_USDC", 
+    "USDFL_DAI", 
+    "USDFL_USDT", 
+    "USDFL_USDN", 
+    "FL_USDC", 
+    "FL_USDT", 
+    "FL_DAI", 
+    "FL_USDN",
+    "USDFL_FL"
+];
